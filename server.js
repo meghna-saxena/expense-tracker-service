2231c8141e488a457c028d3f5934595644e73284
Meghna Srivastava <meghna.s1906@gmail.com>
2020-05-19 13:04:56 +0200

    connect to db

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const dotenv = require('dotenv'); // for env variables
 const colors = require('colors'); // for colors in console
 const morgan = require('morgan'); // for logging
+const connectDB = require('./config/db');
 
 dotenv.config({ path: './config/config.env' });
+
+connectDB();
 
 const transactions = require('./routes/transactions');
 
