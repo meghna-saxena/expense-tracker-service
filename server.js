387af244451155afdaef7a6223a2304ad70dac39
Meghna Srivastava <meghna.s1906@gmail.com>
2020-05-19 09:42:21 +0200

    extracted routes in diff folder

--- server.js
+++ server.js
@@ -5,9 +5,11 @@ const morgan = require('morgan'); // for logging
 
 dotenv.config({ path: './config/config.env' });
 
+const transactions = require('./routes/transactions');
+
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+app.use('/api/v1/transactions', transactions);
 
 const PORT = process.env.PORT || 5000;
 
